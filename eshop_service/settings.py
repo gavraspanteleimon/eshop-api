acb22e44059687b1a240159b59201db06a1847a7
Pantelis <gavraspantelis@gmail.com>
2026-09-22 10:22:07 +0300

    connect Django to PostgreSQL with psycops2-binary

--- eshop_service/settings.py
+++ eshop_service/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'eshop',
+    'rest_framework'
+    #'drf_spectacular',  # Add drf_spectacular to the installed apps for swagger documentation
+    
 ]
 
 MIDDLEWARE = [
@@ -74,8 +78,12 @@ WSGI_APPLICATION = 'eshop_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE' : 'django.db.backends.postgresql',
+        'NAME' : 'eshop_db',
+        'USER' : 'eshop',
+        'PASSWORD' : 'eshop123',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
     }
 }
 
