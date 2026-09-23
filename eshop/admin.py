11bbcd8dd14229f765afc6a3888701476fac1e01
Pantelis <gavraspantelis@gmail.com>
2026-09-23 09:18:13 +0300

    admin finalized

--- eshop/admin.py
+++ eshop/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
+from eshop.models import Category, Product, Cart, CartItem, Order, OrderItem
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'stock', 'is_active')
+    search_fields = ('name','category__name',)
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+    search_fields =('user__username',)
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('cart','product','quantity')
+    search_fields = ('cart__user__username','product__name',)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user','status','created_at','total')
+    search_fields = ('user__username','status',)
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order','product','quantity','unit_price')
+    search_fields = ('order__user__username','product__name',)
+
+
+
 # Register your models here.
--- eshop/models.py
+++ eshop/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True, max_length =100) # friendly URL for category
+
+    def __str__(self):
+        return self.name
+
+    def save(self,*args,**kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args,**kwargs)
+
+class Product(models.Model):
+    name = models.CharField(max_length = 100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10 , decimal_places=2)
+    category = models.ForeignKey(Category , on_delete=models.CASCADE)
+    stock = models.PositiveIntegerField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+class Cart(models.Model):
+    user = models.OneToOneField(User , on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Cart {self.id}"
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name} in {self.cart.id}"
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete =models.CASCADE)
+    status = models.CharField(max_length=20, choices=[('pending', 'Pending'), ('completed', 'Completed'), ('canceled', 'Canceled')], default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2,default=0.00)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.user.username}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product , on_delete= models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantity} x  {self.product.name} in order { self.order.id}"
